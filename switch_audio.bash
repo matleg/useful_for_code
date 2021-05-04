af51a37d616c43fe4550adeb8c3c7c6231a66d44
matleg <ml971@hotmail.com>
2021-05-04 22:47:33 +0200

    add audio commands

--- switch_audio.bash
+++ switch_audio.bash
@@ -2,6 +2,12 @@
 
 # from: https://unix.stackexchange.com/questions/62818/how-can-i-switch-between-different-audio-output-hardware-using-the-shell
 
+#pacmd list-sinks
+#pacmd list-cards
+#alias audio-hdmi='pacmd set-card-profile 0 output:hdmi-stereo+input:analog-stereo'
+#alias audio-laptop='pacmd set-card-profile 0 output:analog-stereo+input:analog-stereo'
+
+
 CURRENT_PROFILE=$(pacmd list-cards | grep "active profile" | cut -d ' ' -f 3-)
 
 if [ "$CURRENT_PROFILE" = "<output:hdmi-stereo>" ]; then
